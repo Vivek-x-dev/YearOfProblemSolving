2bdd268c10942cf9c75a237e596318cd7feea54a
Vivek <vivekdaviet@gmail.com>
2026-01-02 15:14:00 +0530

    binary to decimal

---
+++ Day01.cpp
@@ -0,0 +1,30 @@
+/* Problem 
+
+ You are given a binary number as a string.
+Convert it to its decimal (base-10) equivalent. */
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int ans = 0;
+    int power = 1;
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+        if (s[i] == '1') {
+            ans += power;
+        }
+        power = power * 2;
+    }
+
+    cout << ans;
+    return 0;
+}
+
+/* if anyone want to share there opinion
+ or want to suggest any optimised approach please do share in community.*/
