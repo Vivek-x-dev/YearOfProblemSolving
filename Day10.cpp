5fcdd3ffbc31a596c1faad03d31e77bd13139494
Vivek <vivekdaviet@gmail.com>
2026-01-14 18:13:01 +0530

    Selection sort Problem

---
+++ Day10.cpp
@@ -0,0 +1,37 @@
+/*
+Question : sort an array using Selection sort.
+*/
+#include<iostream>
+using namespace std;
+
+void selectionSort(int *arr ,int n){
+
+   for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
+        }
+  }
+}
+
+
+int main(){
+    int arr[]={11,3,6,14,18,23,9,15};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    selectionSort(arr , n);
+
+     cout<<"sorted array is : " ;
+      for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
