fa43e416514c99d7aaa43a2f25091cd355af8c2f
Vivek <vivekdaviet@gmail.com>
2026-03-11 18:52:15 +0530

    sudoku

--- Day38.cpp
+++ Day38.cpp
@@ -4,25 +4,78 @@ Question : Sudoku problem
 #include <iostream>
 using namespace std;
 
-bool isSafe(int grid[9][9], int row , int col , int num){
-    for (int i=0; i<9  ;i++)
-        if(grid[row][i])          //for col
-            return false;
+bool isSafe(int grid[9][9], int row, int col, int num) {
 
+                                  
+    for(int x = 0; x < 9; x++)
+        if(grid[row][x] == num)
+            return false;               // row check
 
-    for (int j=0; j<9  ; j++)
-        if(grid[col][j])           //for row
-            return false;
+   
+    for(int x = 0; x < 9; x++)
+        if(grid[x][col] == num)
+            return false;                 // column check
 
-    int startRow =row -row%3 ;
-    int startCol =col -col%3 ;
+   
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;           // 3x3 box check
 
-    for (int i=0 ; i<3 ; i++){
-        for(int j=0 ; j<3 ; j++)       //3x3 box check
-            if (grid[i+startRow][j+startCol] == num)
-                return false ;
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++)
+            if(grid[i + startRow][j + startCol] == num)
+                return false;
 
-     return true ;
-    } 
+    return true;
 }
 
+bool solveSudoku(int grid[9][9]) {
+
+    for(int row = 0; row < 9; row++) {
+        for(int col = 0; col < 9; col++) {
+
+            if(grid[row][col] == 0) {
+
+                for(int num = 1; num <= 9; num++) {
+
+                    if(isSafe(grid, row, col, num)) {
+
+                        grid[row][col] = num;
+
+                        if(solveSudoku(grid))
+                            return true;
+
+                        grid[row][col] = 0; // backtrack
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int main() {
+
+    int grid[9][9] = {
+        {3,0,6,5,0,8,4,0,0},
+        {5,2,0,0,0,0,0,0,0},
+        {0,8,7,0,0,0,0,3,1},
+        {0,0,3,0,1,0,0,8,0},
+        {9,0,0,8,6,3,0,0,5},
+        {0,5,0,0,9,0,6,0,0},
+        {1,3,0,0,0,0,2,5,0},
+        {0,0,0,0,0,0,0,7,4},
+        {0,0,5,2,0,6,3,0,0}
+    };
+
+    solveSudoku(grid);
+
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++)
+            cout << grid[i][j] << " ";
+        cout << endl;
+    }
+}
+// i learned how important is writing clean code in big  codes 
