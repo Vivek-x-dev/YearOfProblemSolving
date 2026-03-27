b3d5af961e26154600e33407ddce983a154988a5
Vivek <vivekdaviet@gmail.com>
2026-03-28 02:01:14 +0530

    last problem as for now

--- Day40.cpp
+++ Day40.cpp
@@ -11,12 +11,21 @@ int main(){
         {4,5,6},
         {7,8,9}
     };
+    int row =3 , col =3;
 
-for (int i = 0 ; i< 3 ; i++){
-    if ( i % 2 == 0 ){
-        for (int j=0 ; j< 3 ; j++)
-            cout <<arr[i][j] <<" ";
-    }
-        for
+    for (int i = 0 ; i< row ; i++){
+        if ( i % 2 == 0 ){
+            for (int j=0 ; j< 3 ; j++){
+                cout <<arr[i][j] <<" ";
+            }
+        }
+
+        else{
+            for (int j=col-1 ; j<= 1 ; j--){
+                cout <<arr[i][j] <<" ";
+            }
+        }
+    } 
 }
-} 
+// i am taking break from this because i am seriously preparing for devops intern 
+// i will continue this after getting intership ........
